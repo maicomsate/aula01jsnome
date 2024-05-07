457c16c95f119b2be554f5421741b50bb2c02a75
Michaelsate <159037217+maicomsate@users.noreply.github.com>
2024-05-07 11:59:07 -0300

    aula js 02
    exercicio 01

---
+++ frontend-editor-2024-05-07T14_57_19.663Z/index.js
@@ -0,0 +1,22 @@
+const button = document.querySelector('button')
+
+  button.addEventListener('click',function(e){
+    console.log(e)
+    document.querySelector('h1').classList.toggle('sumiu')
+
+
+  })
+
+
+  var nome = promp('Qual o seu nome?')
+  var name = document.querySelector('p'{
+    nome.
+
+
+
+
+
+  })
+
+  
+   
